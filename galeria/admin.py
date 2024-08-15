c551b0aa713c13a796887c2b3dd9c5db0eba22c6
Rennan Soares Smaniotto <soaressmaniotto@hotmail.com>
2024-08-15 20:01:20 -0300

    Adicionando novos campos no BD

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from galeria.models import Fotografia
 
+@admin.action(description="Marque as selecionadas como publicadas")
+def marque_publicada(modeladmin, request, queryset):
+    queryset.update(publicada=True)
+
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ('id', 'nome', 'legenda')
     search_fields = ('nome', )
+    list_filter = ('categoria', )
+    list_editable = ('publicada', )
+    actions = (marque_publicada, )
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
